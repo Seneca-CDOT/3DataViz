fe2c22d6090a605745da92b053ca99a8c4b82105
Koji Miyauchi <koji@CDOTs-MacBook-Pro.local>
2015-02-24 18:07:07 -0500

    working on mongodb

--- Application.js
+++ Application.js
@@ -1,16 +1,25 @@
-
-require(['SourceCode/Views/GlobeView', 'SourceCode/Models/GlobeModel'], function() {
+require(['SourceCode/Views/GlobeView', 'SourceCode/Models/GlobeModel', 'SourceCode/Models/TweetModel', 'SourceCode/Models/OscarsTweets'] , function() {
 	require(['SourceCode/Controllers/GlobeController'], function() {
-	    var Application = Backbone.View.extend({
-	        initialize: function(options) {
-	            this.globeController = new GlobeController();
-	        },
-	        main: function() {
-	            this.globeController.presentGlobe();
-	        }
-	    });
+	    // var Application = Backbone.View.extend({
+	    //     initialize: function(options) {
+	    //         this.globeController = new GlobeController();
+	    //     },
+	    //     main: function() {
+	    //         this.globeController.presentGlobe();
+	    //     }
+	    // });
 
-	    var application = new Application();
-		application.main();
+	    // var application = new Application();
+
+	    var tweet1 = new Tweet("Koji Miyauchi", "Hello men!!!");
+	    var tweet2 = new Tweet("Kenta Kasahara", "Hello!!!");
+
+	    var tweets = new OscarsTweets();
+	    tweets.add(tweet1);
+	    tweets.add(tweet2);
+	    tweets.remove(tweet2);
+	    console.log(tweets);
+		// application.main();
+		// 
 	});
 });
