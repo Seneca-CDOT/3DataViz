cf23fd18a70cbc43a4b8c577c83b2f202f7d1594
Koji Miyauchi <koji@CDOTs-MacBook-Pro.local>
2015-06-03 11:41:56 -0400

    CORS setting

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ app.all('/tweets/apple', function(req, res) {
 });
 
 http.listen(port);
+http.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', "http://localhost:8000");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
 io.on('connection', function(socket){
 
   /**
