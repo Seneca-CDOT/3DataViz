b77d7b9c57b353c147965ae864b7020577a6f54e
Koji Miyauchi <koji@CDOTs-MacBook-Pro.local>
2015-06-03 13:44:32 -0400

    ws

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
-// Start up server to Listen a request
-var express = require('express');
-var app  = express();
-var http = require('http').Server(app);
-var MongoClient = require('mongodb').MongoClient;
-var io = require('socket.io')(http);
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 var kfs = require('fs');
-var port = process.env.PORT || 7777;
-var twitter = require('twitter');
-var keys;
+var Twitter = require('twitter');
+var MongoClient = require('mongodb').MongoClient;
 var client;
 
 kfs.readFile('keys.json', 'utf8', function(err, data) {
   keys = JSON.parse(data);
-  client = new twitter({ consumer_key: keys.consumer_key, consumer_secret: keys.consumer_secret, access_token_key: keys.token, access_token_secret: keys.token_secret });
+  client = new Twitter({ consumer_key: keys.consumer_key, consumer_secret: keys.consumer_secret, access_token_key: keys.token, access_token_secret: keys.token_secret });
 });
 
-app.listen(port);
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:8000");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+var wss = new WebSocketServer({server: server})
+wss.on("connection", function(ws) {
+  console.log("websocket connection open")
+  ws.onmessage = function (event) {
+    
+    var data = JSON.parse(event.data);
+    switch(data.type){
+      case "start":
+
+        var obj = {};
+        var count =0;
+        if(data.track) obj.track = data.track;        
+        client.stream('statuses/filter',
+          obj, function(stream) {
+          stream.on('data', function(tweet) {
+            if(tweet.coordinates){
+              var msg = {
+                "type": "tweets",
+                "data": tweet
+              }
+              ws.send(JSON.stringify(msg));
+            }
+          });
+          stream.on('end', function() {
+            console.log("end");
+          });
+          stream.on('error', function(error) {
+            throw error;
+          });
+        });
+        break;
+
+      case "stop":
+
+        break;
+    }
+  };
+  
+  ws.on("close", function() {
+    
+    process.exit();
+    // client.stream.end();
+    // client.stream.destroy();
+    console.log("websocket connection close")
+  })
+
 });
-app.use(express.static('../public'));
+
 app.all('/tweets/apple', function(req, res) {
   
   MongoClient.connect('mongodb://'+keys.user+':'+keys.key+'@ds043062.mongolab.com:43062/heroku_app37412051', function(err, db) {
@@ -38,50 +80,6 @@ app.all('/tweets/apple', function(req, res) {
       db.close();
     });
 
-  });
-
-});
-
-http.listen(port);
-http.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:8000");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
-io.on('connection', function(socket){
-
-  /**
-   * socket.on('start')
-   * Give a object includes filter keywords, language.
-   * @param  {Object} data) filtering data.
-   */
-  socket.on('start', function (data) {
-    console.log("start");
-    var obj = {};
-    if(data.track) obj.track = data.track;
-    if(data.language) obj.language = data.language;
-
-    client.stream('statuses/filter',
-      obj, function(stream) {
-      stream.on('data', function(tweet) {
-        if(tweet.coordinates){
-          console.log(tweet);
-          socket.emit('tweet', tweet);
-        }
-      });
-      stream.on('end', function() {
-        console.log("end");
-      });
-      stream.on('error', function(error) {
-        throw error;
-      });
-    });
-
-  });
-  socket.on('stop', function (data) {
-    process.exit();
   });
 
 });
