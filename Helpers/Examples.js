1ea8d1c3fb9394bb11fbad29e17a8e45ead057a0
Dmitry Yastremskiy <Hamabama@Dmitrys-MacBook-Air.local>
2015-10-12 11:00:09 -0400

    example of quakes executed by command

--- Helpers/Examples.js
+++ Helpers/Examples.js
@@ -7,8 +7,8 @@ Application.Examples = {
     var _this = this;
 
     Application.userConfig.model = 'json';
-// Application._vent.trigger('visualize'); // init globe
 
+    return this;
 
   },
   destroy: function() {
@@ -18,10 +18,20 @@ Application.Examples = {
 
     init: function() {
 
-  Application.attrsMap = {date: "time", latitude: "latitude", longitude: "longitude"};
-     Application.userConfig.template = 'dynamic';
-     Application._vent.trigger('controlpanel/parse'); // create collection
+      Application._vent.on('data/ready', this.callTimeline, this);
 
+      Application.attrsMap = {date: "time", latitude: "latitude", longitude: "longitude"};
+      Application.userConfig.template = 'dynamic';
+      Application._vent.trigger('controlpanel/parse'); // create collection
+
+    },
+    destroy: function() {
+      Application._vent.unbind('data/ready', this.callTimeline);
+
+    },
+    callTimeline: function() {
+
+      Application._vent.trigger('timeline/on');
 
     },
 
